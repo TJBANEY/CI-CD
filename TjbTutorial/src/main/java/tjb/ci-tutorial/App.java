e9756c029e0ccb383174ea5d4ebaf2c83317f565
Timothy Baney <baney-surv@hotmail.com>
2018-12-01 20:09:57 -0600

    Start Maven project

---
+++ TjbTutorial/src/main/java/tjb/ci-tutorial/App.java
@@ -0,0 +1,13 @@
+package tjb.ci-tutorial;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
